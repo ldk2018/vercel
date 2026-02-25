b9df410a161c987eea19479e57c467aead13a664
ldk2018 <ldk836@outlook.com>
2026-02-25 16:14:37 +0800

    Update report.js

--- api/report.js
+++ api/report.js
@@ -1,3 +1,1 @@
-export default function handler(req, res) {
-  res.status(200).json({ message: 'Hello from Vercel' });
-}
+javascript<br>export default function handler(req, res) {<br> res.status(200).json({ message: 'API is working' });<br>}<br>
